458f251f995c7be7973d7d20cee28926286ac620
Kexin Xu <kexinxu@gwu.edu>
2019-06-08 18:04:33 -0400

    Add files via upload

---
+++ Laioffer Code/SortingAlgorithm-SelectionSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SortingAlgorithm {
+    public static void main(String[] args) {
+        SortingAlgorithm solution = new SortingAlgorithm();
+        int[] test = {2, 4, 5, 7, 1, 2, 3, 6};
+        int[] result = solution.SelectionSort(test);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    public int[] SelectionSort(int[] array) {
+
+        int length = array.length;
+
+        for (int left = 0; left < length; left++) {
+            int M = left;
+            for (int right = left + 1; right < length; right ++) {
+                if (array[right] < array[M]) {
+                    M = right;
+                }
+            }
+            int help = array[left];
+            array[left] = array[M];
+            array[M] = help;
+        }
+        return array;
+    }
+}
+
