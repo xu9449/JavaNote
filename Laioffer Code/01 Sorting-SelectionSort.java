18d8f04d2b7d68fa24469760a689d731072faa8c
Kexin Xu <kexinxu@gwu.edu>
2019-06-15 06:36:48 -0400

    Update 01 Sorting-SelectionSort.java

--- Laioffer Code/01 Sorting-SelectionSort.java
+++ Laioffer Code/01 Sorting-SelectionSort.java
@@ -10,10 +10,13 @@ public class SortingAlgorithm {
     }
 
     public int[] SelectionSort(int[] array) {
-
+        //check null before any other things
+        // check other conditions
+        if (array = null || array.length <= 1) {// 此处array.length <= 1 更好          return array;
+        }
         int length = array.length;
 
-        for (int left = 0; left < length; left++) {
+        for (int left = 0; left < length - 1; left++) { //此处注意是length - 1
             int M = left;
             for (int right = left + 1; right < length; right ++) {
                 if (array[right] < array[M]) {
