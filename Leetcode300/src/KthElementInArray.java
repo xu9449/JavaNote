975ab0043ff8989b2451a4935cfdcc59242ff2e1
xu9449 <xu9449@gmail.com>
2019-11-18 23:48:40 -0500

    update

--- Leetcode300/src/KthElementInArray.java
+++ Leetcode300/src/KthElementInArray.java
@@ -1,2 +1,10 @@
 public class KthElementInArray {
+    public class KSmallest {
+        public int[] kSmallestI(int[] array, int k) {
+            if (array.length == 0 || k == 0) {
+                return new int[0];
+            }
+        }
+    }
+
 }
