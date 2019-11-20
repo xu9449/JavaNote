df72e4c48d1d09b4e667c65b03b01b6ee12f2164
xu9449 <xu9449@gmail.com>
2019-11-19 19:06:47 -0500

    update

--- Leetcode300/src/KthElementInArray.java
+++ Leetcode300/src/KthElementInArray.java
@@ -1,9 +1,34 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class KthElementInArray {
     public class KSmallest {
         public int[] kSmallestI(int[] array, int k) {
             if (array.length == 0 || k == 0) {
                 return new int[0];
             }
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    if(o1.equals(o2)) {
+                        return 0;
+                    }
+                    return o1 > o2 ? -1 : 1;
+                }
+            });
+            for (int i = 0; i < array.length; i++) {
+                if (i < k) {
+                    maxHeap.offer(array[i]);
+                } else if (array[i] < maxHeap.peek()) {
+                    maxHeap.poll();
+                    maxHeap.offer(array[i]);
+                }
+            }
+            int[] result = new int[k];
+            for (int i = k - 1; i >= 0; i--) {
+                result[i] = maxHeap.poll();
+            }
+            return result;
         }
     }
 
