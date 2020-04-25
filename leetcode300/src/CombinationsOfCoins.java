7d87ec4dd6d180d1f1096bc2c72155eef834e531
xu9449 <xu9449@gmail.com>
2020-04-25 13:55:17 -0400

    update

--- leetcode300/src/CombinationsOfCoins.java
+++ leetcode300/src/CombinationsOfCoins.java
@@ -4,36 +4,43 @@ import java.util.List;
 public class CombinationsOfCoins {
     public static void main(String[] args) {
         CombinationsOfCoins solu = new CombinationsOfCoins();
-        int[] coins = {25, 10, 5, 1};
-        int target = 50;
-        List<List<Integer>> result =  solu.combinations(target, coins);
+        int[] coins = {2, 3, 6, 7};
+        int target = 7;
+        List<List<Integer>> result =  solu.combinationSum(coins, target);
         System.out.println(result);
 
     }
-    public List<List<Integer>> combinations(int target, int[] coins) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int[] level = new int[candidates.length];
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         List<Integer> cur = new ArrayList<Integer>();
-        findComb(target, coins, 0, cur, result);
+        combine(candidates, cur, 0, target, result);
         return result;
     }
 
-    public void findComb(int target, int[] coins, int index, List<Integer> cur,
-                                        List<List<Integer>> result){
-        if(index == coins.length - 1){
-            if(target%coins[coins.length - 1] == 0){
-                cur.add(target/coins[coins.length - 1]);
-                result.add(new ArrayList<Integer>(cur));
+    public void combine(int[] candidates, List<Integer> cur, int index, int left, List<List<Integer>> result){
+        if(index == candidates.length - 1){
+            if(left%candidates[index] == 0){
+                cur.add(left/candidates[index]);
+                List<Integer> list = new ArrayList<>();
+                for(int i = 0; i < cur.size() ; i++) {
+                    if(cur.get(i) != 0){
+                        for(int j = 1; j <= cur.get(i); j++){
+                            list.add(candidates[i]);
+                        }
+                    }
+                }
+                result.add(new ArrayList<Integer>(list));
                 cur.remove(cur.size() - 1);
             }
             return;
         }
-        int max = target / coins[index];
+        int max = left / candidates[index];
         for(int i = 0; i <= max; i++) {
             cur.add(i);
-            findComb(target - i * coins[index], coins, index + 1, cur, result);
+            combine(candidates, cur, index + 1, left - i * candidates[index], result);
             cur.remove(cur.size() - 1);
         }
-
     }
 
 }
