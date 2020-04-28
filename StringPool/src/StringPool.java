9a9e76a59f89fef418fa22e61a7a536fe669d3b9
xu9449 <xu9449@gmail.com>
2020-04-28 17:14:17 -0400

    update

---
+++ StringPool/src/StringPool.java
@@ -0,0 +1,18 @@
+public class StringPool {
+    public static void main(String[] args) {
+        String s1 = "Test";
+        String s2 = "Test";
+        String s3 = new String("Test");
+        final String s4 = s3.intern();
+        System.out.println(s1 == s2);
+        System.out.println(s2 == s3);
+        System.out.println(s3 == s4);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s4);
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(s3));
+        System.out.println(s3.equals(s4));
+        System.out.println(s1.equals(s4));
+        System.out.println(s1.equals(s3));
+    }
+}
