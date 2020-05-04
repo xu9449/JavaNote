9304b328a86da026c4f99f13265eb35c8ba481b1
xu9449 <xu9449@gmail.com>
2020-05-04 15:05:43 -0400

    update

---
+++ CountWords/src/CountWords.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+        Integer a = null;
+        System.out.println(a);
+    }
+}
