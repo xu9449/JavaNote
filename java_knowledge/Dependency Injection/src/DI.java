77fffaa9b249181f7a2f675ea943c4e309550989
xu9449 <xu9449@gmail.com>
2020-06-23 23:59:31 -0400

    add the begin of the DI

--- java_knowledge/Dependency Injection/src/DI.java
+++ java_knowledge/Dependency Injection/src/DI.java
@@ -1,3 +1,23 @@
+/*
+DI ways:
+constructor based injection:
+
+setter based: only different is the 顺序
+
+filed based injection:
+cons: easy to find the dependency
+pros: break all the encapsulation things
+
+Dependency Injection advantage:
+i. code loose couple
+ii. easily testable
+iii. moure reusable as the implementation is in the java configuration files
+
+ */
+
 public class DI {
+    public static void main(String[] args) {
+
+    }
 
 }
