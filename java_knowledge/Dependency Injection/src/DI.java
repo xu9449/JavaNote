2dfeac0233b740ba86e4ae3989b2ef320d5d19d7
xu9449 <xu9449@gmail.com>
2020-06-24 00:19:16 -0400

    finished DI

--- java_knowledge/Dependency Injection/src/DI.java
+++ java_knowledge/Dependency Injection/src/DI.java
@@ -1,4 +1,6 @@
 /*
+DependencyInjection Note:
+
 DI ways:
 constructor based injection:
 
@@ -13,6 +15,14 @@ i. code loose couple
 ii. easily testable
 iii. moure reusable as the implementation is in the java configuration files
 
+Disadvantage:
+i. 拆的太碎， increases complexity, usually by increasing the number of classes
+ii. Your code will coupled to the dependency injection framework you use
+if you change to another framework, you will be really hard
+iii. DI containers or approaches that perform type resolving generally incur a slight runtime penalty
+
+Prototype:
+在原来的基础上进行修改，比如getClaringClass()
  */
 
 public class DI {
